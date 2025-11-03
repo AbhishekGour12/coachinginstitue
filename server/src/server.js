0bc42edf1a2c8a0c89238318527d2f34869b1ed0
AbhishekGour12 <aggour124421@gmail.com>
2025-11-03 15:02:07 +0530

    folder structure  updated

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import express from 'express';
+import connectDB from './config/db.js';
+import authRoutes from './routes/AuthRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+connectDB()
+
+app.use('/api/auth', authRoutes);
+
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+})
